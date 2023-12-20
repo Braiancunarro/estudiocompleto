8fbd69b69ed254e0d3f89d636d4ed615dbaeb54c
Braian <braian.cunarro@gmail.com>
2023-12-19 22:22:24 -0300

    initComit

---
+++ app/src/main/java/com/aristidevs/estudiocompleto/InyeccionDeDependencias/General.kt
@@ -0,0 +1,24 @@
+package com.aristidevs.estudiocompleto.InyeccionDeDependencias
+
+//Inyeccion de dependencias
+
+// Es un patron de diseño que se utiliza para facilitar la creacion y gestion de dependencias en una aplicación,
+
+
+
+// DAGGER HILT
+//Es una extension de Dagger 2, la biblioteca de inyeccion de dependencias para JAVA y android.
+//Fue desarrollada por google para simplificar la implementacion.
+//Para usarlo hay que agregar las dependencias adecuadas en build.gradle
+
+//dependencias { implementation ("com.google.dagger:hilt-android:2.x")
+//                 kapt ("com.google.dagger:hilt-android-compiler:2.x")
+//                 implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+//                 kapt ("androidx.hilt:hilt-compiler:1.0.0")}
+
+//luego anotar la aplicacion con @HiltAndroidApp en la clase MyApplication: Application()
+
+//
+
+class General {
+}
